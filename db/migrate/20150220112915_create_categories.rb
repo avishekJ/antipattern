2f50ba472ffe1775ebdfb792ee5f73e20504d5f0
avishekJ <avishekjana.it@gmail.com>
2015-02-23 15:59:58 +0530

    post, subject, category revamped

--- db/migrate/20150220112915_create_categories.rb
+++ db/migrate/20150220112915_create_categories.rb
@@ -4,11 +4,14 @@ class CreateCategories < ActiveRecord::Migration
       t.string 	:name, :limit => 25, :null => false
       t.text 	:description
       t.integer :subject_id, :null => false
+      t.string :permalink, :null => false
       t.integer :parent_id
       t.boolean :visible, :default => false
+
       t.timestamps null: false
     end
     add_index('categories', 'subject_id')
+    add_index("categories", "permalink")
   end
 
   def down 
--- db/migrate/20150220114644_create_subjects.rb
+++ db/migrate/20150220114644_create_subjects.rb
@@ -4,9 +4,10 @@ class CreateSubjects < ActiveRecord::Migration
       t.string :name, :null => false, :limit => 25
       t.integer :position
       t.boolean :visible, :default => false
-
+      t.string :permalink, :null => false
       t.timestamps null: false
     end
+    add_index("subjects", "permalink")
   end
 
   def down
